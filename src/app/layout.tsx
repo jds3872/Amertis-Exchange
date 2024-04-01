fa8c1ee2e6b1d3fe0bf88796b632400c60880c44
gwilldan <sayhi2godswill@gmail.com>
2024-04-01 18:58:53 +0100

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import { Inter, Tienne } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import {Nav, Footer} from "@/components/reusableComp/index"
+import {Nav, Footer} from "@/components/reusableComp"
 
 const inter = Inter({ subsets: ["latin"] });
 
